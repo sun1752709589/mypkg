fb63545bfc14f589fb196c23b63c6b765a7f41e9
NASA <email4sun@163.com>
2019-09-11 17:39:38 +0800

    fix

--- mypkg/tools/__init__.py
+++ mypkg/tools/__init__.py
@@ -1,12 +1,2 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
-"""
-    __init__.py
-    ~~~~ on September 07,2018 10:03
-    
-    The description is written here.
-    
-    :license MIT, see LICENSE for more details.
-    :copyright (c) 2013 - 2018 by LauCyun<liu@laucyun.com>.
-"""
